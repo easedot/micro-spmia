95d03a14acb40ba300e85e363fc77523df369184
easedot <jhh@easedot.com>
2019-06-10 07:54:09 +0800

    add deploy config

--- travis_scripts/deploy_to_docker_hub.sh
+++ travis_scripts/deploy_to_docker_hub.sh
@@ -1,8 +1,8 @@
 echo "Pushing service docker images to docker hub ...."
-echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin
-docker push johncarnell/tmx-authentication-service:$BUILD_NAME
-docker push johncarnell/tmx-licensing-service:$BUILD_NAME
-docker push johncarnell/tmx-organization-service:$BUILD_NAME
-docker push johncarnell/tmx-confsvr:$BUILD_NAME
-docker push johncarnell/tmx-eurekasvr:$BUILD_NAME
-docker push johncarnell/tmx-zuulsvr:$BUILD_NAME
+docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
+docker push easedot/tmx-authentication-service:$BUILD_NAME
+docker push easedot/tmx-licensing-service:$BUILD_NAME
+docker push easedot/tmx-organization-service:$BUILD_NAME
+docker push easedot/tmx-confsvr:$BUILD_NAME
+docker push easedot/tmx-eurekasvr:$BUILD_NAME
+docker push easedot/tmx-zuulsvr:$BUILD_NAME
