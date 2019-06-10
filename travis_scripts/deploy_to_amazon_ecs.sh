4494cc59c0a80e2adff0ca95597de84f3d05fa0e
easedot <jhh@easedot.com>
2019-06-10 14:58:11 +0800

    modify penalty order error

--- travis_scripts/deploy_to_amazon_ecs.sh
+++ travis_scripts/deploy_to_amazon_ecs.sh
@@ -1,4 +1,4 @@
 echo "Launching $BUILD_NAME IN AMAZON ECS"
-ecs-cli configure --region us-west-2 --access-key $AWS_SECRET_ACCESS_KEY --secret-key $AWS_SECRET_ACCESS_KEY --cluster MICRO-DEPLOY
+ecs-cli configure --region us-west-2  --cluster MICRO-DEPLOY
 ecs-cli compose --file docker/common/docker-compose.yml up
 rm -rf ~/.ecs
